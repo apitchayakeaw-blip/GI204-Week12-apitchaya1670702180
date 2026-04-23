f15ae3ae857831122907d867e3ff76787e7f448c
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-04-23 21:12:44 +0700

    Week12

---
+++ Assets/Settings/Scripts/Player2DController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player2DController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float jumpForce = 450;
+    public bool isGrounded;
+    private SpriteRenderer spriteRenderer;
+
+    private Rigidbody2D rb;
+    private float moveInputValue;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current != null)
+        {
+            moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) 
+                - (Keyboard.current.aKey.isPressed ? 1 : 0);
+        }
+        rb.linearVelocity = new Vector2(moveInputValue * speed, rb.linearVelocity.y);
+        //if (moveInputValue < 0) { spriteRenderer.flipX = true; }
+        //else if (moveInputValue > 0) { spriteRenderer.flipX = false; }
+
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded) 
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce )); 
+        }
+
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+
+    }
+
+
+}
